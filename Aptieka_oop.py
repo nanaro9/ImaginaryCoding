43a6af70a3c35d4d40140f2eacb82fad0cfd09c8
PoalChaynik <alpolaop13@gmail.com>
2023-09-25 15:23:20 +0300

    aptieka_oop (sakums)

--- Aptieka_oop.py
+++ Aptieka_oop.py
@@ -2,5 +2,14 @@
 # Programmas izstrādātājs - Aleksis Počs
 
 class Aptieka():
+    # Konstruktora izveide
     def __init__(self):
-        pass
+        self.Antibiotiku_kategorija = ""
+        self.Antibiotiku_nosaukums = ""
+        self.Antibiotiku_raksturojums = ""
+        self.Antibiotiku_cena = ""
+        self.Pirceja_vārds= ""
+        self.Pirceja_uzvārds = ""
+        self.Priceja_pk = ""
+        self.Pirceja_mobilais = ""
+
